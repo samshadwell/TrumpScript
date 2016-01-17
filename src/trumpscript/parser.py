c3aabf1c7ce8e4ed569c596aba5f3f46f27dfebf
Samuel Shadwell <smshdwll@gmail.com>
2016-01-17 03:43:51 -0600

    Merged Chris's stuff.

--- src/trumpscript/parser.py
+++ src/trumpscript/parser.py
@@ -1,11 +1,12 @@
 # Parser for TrumpScript
 # 1/16/2016
 
+from ast import *
+
 from src.trumpscript.constants import *
 
 
 class Parser:
-
     def parse(self, tokens):
         tokens = self.pre_parse(tokens)
         body_list = []
@@ -37,16 +38,16 @@ class Parser:
         def handle_make():
             token = consume(T_Make)
             variable = consume(T_Word)
-            followup = peek()           # Check the type of the next token to see if it's acceptable
+            followup = peek()  # Check the type of the next token to see if it's acceptable
             # TODO: enumerate all the cases here
             pass
 
         ##Both Assign and EQ because why the hell not guys
         def handle_is():
-            #TODO: this whole thing
+            # TODO: this whole thing
             pass
 
-        #Print
+        # Print
         def handle_print():
             consume(T_Print)
             followup = peek()
@@ -66,16 +67,16 @@ class Parser:
                 output = "error"
                 # TODO: real errors
 
-            return Call(func=Name(idx="print", ctx=Load),args=output, keywords=[])
+            return Call(func=Name(idx="print", ctx=Load), args=output, keywords=[])
 
-        #While
+        # While
         def handle_while():
             token = consume(T_While)
             conditional = handle_paren()
             body = handle_brace()
             # TODO: build tree from outputs
 
-        #If
+        # If
         def handle_if():
             token = consume(T_If)
             conditional = handle_paren()
@@ -86,7 +87,7 @@ class Parser:
                 orelse = []
             return If(conditional, body, orelse)
 
-        #orelse piece of if
+        # orelse piece of if
         def handle_else():
             consume(T_Else)
             if peek() == T_If:
@@ -94,54 +95,52 @@ class Parser:
             else:
                 return handle_brace()
 
-        #BoolOp(s)
+        # BoolOp(s)
         def handle_boolop():
             # TODO: take care of and / or
             pass
 
-        #BinOp(s)
+        # BinOp(s)
         def handle_binop():
             # TODO: this mess
             pass
 
-        #Not
+        # Not
         def handle_not():
             # TODO: handle not
             pass
 
-        #Compare
+        # Compare
         def handle_compare():
             # TODO: handle compare
             pass
 
-        #Num
+        # Num
         def handle_num():
             token = consume(T_Num)
             num = token["value"]
             return Num(num)
 
-        #Str
+        # Str
         def handle_quote():
             token = consume(T_Quote)
             text = token["value"]
             return Str(text)
 
-        #Name
+        # Name
         def handle_word():
-            #TODO: this whole shit
+            # TODO: this whole shit
             pass
 
-        #True
+        # True
         def handle_true():
             token = consume(T_True)
-            return Name(idx = "True", ctx = Load)
+            return Name(idx="True", ctx=Load)
 
-        #False
+        # False
         def handle_false():
             token = consume(T_False)
-            return Name(idx = "False", ctx = Load)
-
-
+            return Name(idx="False", ctx=Load)
 
         return body_list
 
@@ -149,7 +148,7 @@ class Parser:
     def pre_parse(tokens) -> list:
         tokens = list(tokens)
         t = "type"
-        t_null = {type: -1, "value": "NAN", "line" : "NAN"}
+        t_null = {type: -1, "value": "NAN", "line": "NAN"}
         i = 0
         variables = set()
         token = t_null
@@ -172,6 +171,3 @@ class Parser:
             i += 1
 
         return tokens
-
-
-
